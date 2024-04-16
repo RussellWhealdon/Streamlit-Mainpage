5950f8048ebdb57aea78ea511ea9373599df4e83
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-04-16 15:43:10 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title= f"Russell Whealdon Data Science Portfolio",page_icon="🧑‍🚀",layout="wide")
+st.markdown(f"<h1 style='text-align: center;'>Russell Whealdon Data Science Portfolio</h1>", unsafe_allow_html=True)
+
+#Set background image for page
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background-image: url("https://source.unsplash.com/blue-sky-and-white-clouds-b8dA3eY5VrY");
+background-size: cover;
+}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
