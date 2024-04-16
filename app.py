43a621def603a405fb0084ae6fb9cc3a2db49f58
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-04-16 16:31:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,5 @@ background-size: cover;
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
+
+st.markdown(f"<p style='text-align: center; color: white;'>Hi! I'm Russell Whealdon and this my Data Science portfolio that holds a wide range of interesting Analytics, Machine Learning, and AI related projects. I'm currently a Data Analyst working at an Ad Agency specializing in modeling and data infrastructure. In here you will also find projects from my Masters in Business Analytics program. I hope you find it interesting and would love to hear from you. Please reach out to me directly or connect on Linkedin. Cheers!/p>", unsafe_allow_html=True)
