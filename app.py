f421fad49b7360c27de35a4ad3bc946eb3ac936e
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-06-04 11:28:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,12 @@ with st.container():
 
 
 with st.container():
-    st.markdown(f"<h2 style='text-align: left; color: white;'>Tableau Dashboards</h2>", unsafe_allow_html=True)
-    st.markdown(f"<h4 style='text-align: left; color: white;'>Period over Period Comparison for Account Managers</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: left; color: white;'>Tableau Projects</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='text-align: left; color: white;'>Period over Period Comparison Dashboard</h4>", unsafe_allow_html=True)
     st.markdown(f"<p style='text-align: left; color: white;'>This dashboard uses LOD expressions and dynamic parameters to quickly allow account managers to switch between key metrics to see both period over period statistics and custom visuals.</p>", unsafe_allow_html=True)
     st.image("images/TableauPoPChart.gif")
+    st.write("    ")
+    st.write("    ")
+    st.markdown(f"<h4 style='text-align: left; color: white;'>Creative Testing and Response Rate Dashboard</h4>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align: left; color: white;'>This dashboard uses LOD expressions and dynamic parameters to quickly allow account managers to switch between key metrics to see both period over period statistics and custom visuals.</p>", unsafe_allow_html=True)
+    st.image("images/DatacampTableauDash.jpg")
